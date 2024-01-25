9cfdedda170f7169e6cc24a641e533855a5c7889
Luo Zhongqi <luozhongqi@mail.com>
2024-01-25 13:05:21 +0800

    Finished soundfont parsing.

---
+++ example/readsfinfo.cpp
@@ -0,0 +1,17 @@
+#include "soundfont.h"
+#include <string>
+#include <iostream>
+#include <cstddef>
+#include <cstdint>
+
+
+int main(int argc, char const *argv[])
+{
+    prestosynth::soundfont_internal::SoundFont sf(std::string("../example/MuseScore_General.sf3"));
+    std::cout << sf.get_version() << std::endl;
+    std::cout << sf.pdta().inst_num() << std::endl;
+    std::cout << sf.pdta().inst(1).name << std::endl;
+    std::cout << sf.pdta().phdr(0).name << std::endl;
+
+    return 0;
+};
