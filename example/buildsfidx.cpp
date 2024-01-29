4fcabfd70e3fe38b4a5be44812321da1e69f7e58
Luo Zhongqi <luozhongqi@mail.com>
2024-01-29 22:56:20 +0800

    Working on Soundfont wrapper

---
+++ example/buildsfidx.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+
+#include "audiodata.h"
+#include "soundfont.h"
+
+int main(int argc, char const *argv[])
+{
+    prestosynth::PrestoSoundFont sf(std::string("../example/MuseScore_General.sf3"), 44100);
+
+    return 0;
+};
