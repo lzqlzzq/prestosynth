17b37fb94c12df898e0b5b0881ac08cc0d7f2604
Luo Zhongqi <luozhongqi@mail.com>
2024-02-10 14:56:47 +0800

    Reorganize wav writing

--- include/wav.h
+++ include/wav.h
@@ -1,13 +1,13 @@
 #ifndef _WAV_H
 #define _WAV_H
 
-
 #include <cstdio>
+#include <exception>
+#include <string>
 
 
 namespace prestosynth {
 
-#pragma pack(1)
 struct FMTChunk {
 	uint16_t format;  // 3: float, 1: short
 	uint16_t channelNum;
@@ -16,32 +16,24 @@ struct FMTChunk {
 	uint16_t blockAlign;
 	uint16_t bitPerSample;
 };
-#pragma pack()
+static_assert(sizeof(FMTChunk) == 16);
 
 const FMTChunk FMT_FLOAT = {3, 2, 44100, 4, 8, 32};
 const FMTChunk FMT_SHORT = {1, 2, 44100, 2, 4, 16};
 
-inline void WAVE_write(const char *filename,
+inline void WAVE_write(const std::string &filename,
 	unsigned short num_channels,
 	unsigned long num_samples,
 	int sample_rate,
-	float *data)
-{
-	unsigned int bytes_per_sample;
-	unsigned int byte_rate;
-	unsigned long i;	// counter for samples
+	float *data) {
 	size_t chunkSize = 0;
 
-	bytes_per_sample = 4;
-	byte_rate = sample_rate * num_channels * bytes_per_sample;
-
-	FILE *fp = fopen(filename, "wb");
-	if (!fp) return;
-	//assert(fp);	// make sure it opened
+	FILE *fp = fopen(const_cast<char*>(filename.c_str()), "wb");
+	if (!fp) throw std::ios_base::failure("Cannot write the wav file " + filename + "!");
 
 	// write RIFF header
 	fwrite("RIFF", 1, 4, fp);
-	chunkSize = 36 + bytes_per_sample * num_samples * num_channels;
+	chunkSize = 36 + sizeof(float) * num_samples * num_channels;
 	fwrite(&chunkSize, 1, 4, fp);
 	fwrite("WAVE", 1, 4, fp);
 
@@ -50,15 +42,17 @@ inline void WAVE_write(const char *filename,
 	chunkSize = 16;
 	fwrite(&chunkSize, 1, 4, fp);
 	FMTChunk fmt = FMT_FLOAT;
-	fmt.blockAlign = num_channels * bytes_per_sample;
+	fmt.channelNum = num_channels;
+	fmt.blockAlign = num_channels * sizeof(float);
 	fmt.sampleRate = sample_rate;
+	fmt.byteRate = sample_rate * num_channels * sizeof(float);
 	fwrite(&FMT_FLOAT, sizeof(FMTChunk), 1, fp);
 
 	// write data subchunk
 	fwrite("data", 1, 4, fp);
-	chunkSize = bytes_per_sample * num_samples * num_channels;
+	chunkSize = sizeof(float) * num_samples * num_channels;
 	fwrite(&chunkSize, 1, 4, fp);
-	fwrite(data, 4, num_samples*num_channels, fp);
+	fwrite(data, sizeof(float), num_samples * num_channels, fp);
 
 	fclose(fp);
 };
