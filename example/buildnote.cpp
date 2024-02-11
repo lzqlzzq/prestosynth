eb3fbafacd97ca21d223db007829584bca7fbbac
Luo Zhongqi <luozhongqi@mail.com>
2024-02-12 00:07:53 +0800

    Switch to eigen

--- example/buildnote.cpp
+++ example/buildnote.cpp
@@ -10,7 +10,7 @@ int main(int argc, char const *argv[])
 {
     psynth::PrestoSoundFont sf(std::string("../example/MuseScore_General.sf2"), 44100, 0);
 
-    psynth::AudioData audio = sf.build_note(40, 0, 60, 40, 10.f, false);
+    psynth::AudioData audio = sf.build_note(42, 0, 60, 127, 10.f, true);
 
     psynth::write_audio("test2.wav", audio, 44100);
 
--- include/synthesizer.h
+++ include/synthesizer.h
@@ -1,11 +1,37 @@
 #ifndef _SYNTHESIZER_H
 #define _SYNTHESIZER_H
 
+#include <map>
+#include <queue>
+#include <rigtorp/MPMCQueue.h>
 #include "sequence.h"
 #include "soundfont.h"
 #include "util/audio_util.h"
 
 namespace psynth {
+
+struct NoteHead {
+    uint32_t duration;
+    uint8_t pitch;
+    uint8_t velocity;
+};
+
+typedef std::queue<uint32_t> NoteStartPack;
+
+struct PackedNote {
+    NoteHead head;
+    NoteStartPack startPack;
+};
+
+typedef std::map<NoteHead, NoteStartPack> NoteMap;
+typedef rigtorp::mpmc::Queue<PackedNote> PackedNoteQueue;
+
+struct NoteAudioPack {
+    AudioData audio;
+    NoteStartPack startPack;
+};
+
+typedef rigtorp::mpmc::Queue<NoteAudioPack> NoteAudioQueue;
 
 class Synthesizer {
 private:
