a7160b0eb4bbda3a7e086e4901f2573ad132bd51
lyk <lyk-boya@outlook.com>
2024-02-14 19:09:19 +0800

    fix M_PI on Windows

--- src/filter.cpp
+++ src/filter.cpp
@@ -4,6 +4,10 @@
 
 namespace psynth {
 
+#ifndef M_PI
+#define M_PI  3.14159265358979323846264f  // from CRC
+#endif
+
 LowPassFilter::LowPassFilter(float cutOffFreq, float sampleRate, float Q) {
     double fc = 2 * M_PI * cutOffFreq / sampleRate;
 
