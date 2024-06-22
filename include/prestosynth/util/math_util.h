2377a453b040b5edf16b3f4d9dfa59927a1f4261
lyk <lyk-boya@outlook.com>
2024-06-22 23:41:23 +0800

    Remove unnecessary constexpr tags

--- include/prestosynth/util/math_util.h
+++ include/prestosynth/util/math_util.h
@@ -2,53 +2,53 @@
 #define _MATH_UTIL_H
 
 #include <array>
-#include <cassert>
+// #include <cassert>
 #include <Eigen/Core>
-#include "gcem.hpp"
+// #include "gcem.hpp"
 
 namespace psynth {
 
-constexpr float timecents_to_ms(int16_t timecent) {
+inline float timecents_to_ms(int16_t timecent) {
     return std::pow(2.f, static_cast<float>(timecent) / 1200.f) * 1000.f;
 };
 
-constexpr float timecents_to_s(int16_t timecent) {
+inline float timecents_to_s(int16_t timecent) {
     return std::pow(2.f, static_cast<float>(timecent) / 1200.f);
 };
 
-constexpr int16_t s_to_timecents(float second) {
+inline int16_t s_to_timecents(float second) {
     return 1200 * std::log2(second);
 };
 
-constexpr float db_to_amplitude(float db) {
-    return gcem::pow(10.f, db / 20.f);
+inline float db_to_amplitude(float db) {
+    return std::pow(10.f, db / 20.f);
 };
 
-constexpr float cb_to_amplitude(float cb) {
+inline float cb_to_amplitude(float cb) {
     return std::pow(10.f, cb / 200.f);
 };
 
-constexpr float amplitude_to_cb(float amp) {
+inline float amplitude_to_cb(float amp) {
     return 200 * std::log10(amp);
 };
 
-constexpr float amplitude_to_db(float amp) {
+inline float amplitude_to_db(float amp) {
     return 20 * std::log10(amp);
 };
 
-constexpr uint32_t s_to_frames(float second, float sampleRate) {
+inline uint32_t s_to_frames(float second, float sampleRate) {
     return std::ceil(second * sampleRate);
 };
 
-constexpr float LOG_EPS = db_to_amplitude(-144.f);
+inline float LOG_EPS = db_to_amplitude(-144.f);
 
 namespace math_internal {
 
-constexpr float pitch_to_hz(uint8_t pitch) {
-    return 440 * gcem::pow(2.f, (static_cast<float>(pitch) - 69) / 12);
+inline float pitch_to_hz(uint8_t pitch) {
+    return 440 * std::pow(2.f, (static_cast<float>(pitch) - 69) / 12);
 };
 
-constexpr std::array<float, 128> make_pitch_hz_lut() {
+inline std::array<float, 128> make_pitch_hz_lut() {
     std::array<float, 128> LUT {};
 
     for (int i = 0; i < 128; ++i) {
@@ -58,19 +58,19 @@ constexpr std::array<float, 128> make_pitch_hz_lut() {
     return LUT;
 };
 
-constexpr std::array<float, 128> PITCH_HZ_LUT = make_pitch_hz_lut();
+inline std::array<float, 128> PITCH_HZ_LUT = make_pitch_hz_lut();
 
 }
 
-constexpr float pitch_to_hz(uint8_t pitch) {
+inline float pitch_to_hz(uint8_t pitch) {
     return math_internal::PITCH_HZ_LUT[pitch];
 };
 
-constexpr float semitone_to_tune(float semitones) {
+inline float semitone_to_tune(float semitones) {
     return std::pow(2.f, semitones / 12.f);
 };
 
-constexpr float cent_to_tune(float cent) {
+inline float cent_to_tune(float cent) {
     return std::pow(2.f, cent / 1200.f);
 };
 
@@ -79,7 +79,7 @@ auto cent_to_tune(T cent) {
     return Eigen::pow(2, cent / 1200.f);
 };
 
-constexpr float abscent_to_hz(float absCent) {
+inline float abscent_to_hz(float absCent) {
     return 8.176f * std::pow(2.f, absCent / 1200.f);
 };
 
